63f8bfc7645e5b0a5e4dda15acb743aae88e1b57
Quan Nguyen <minhquan2nd@gmail.com>
2025-07-19 21:22:32 +0700

    add

--- Assets/_Scripts/Network/NetworkMessages/Server/GameState/GameStateUpdate.cs
+++ Assets/_Scripts/Network/NetworkMessages/Server/GameState/GameStateUpdate.cs
@@ -2,6 +2,8 @@ using System.Collections.Generic;
 
 public class GameStateUpdate : ServerMessage
 {
+    public int ProcessedInputSequence;
+    
     public PlayerState[] PlayerStates;
     public GameStateUpdate() : base(NetworkMessageTypes.Server.GameState.StateUpdate) { }
 }
