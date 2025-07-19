ccc7eec3c4495b641067705affffcc335e4bbf15
Quan Nguyen <minhquan2nd@gmail.com>
2025-07-19 21:49:30 +0700

    add

--- Assets/_Scripts/Network/NetworkMessages/Server/Entity/PlayerState.cs
+++ Assets/_Scripts/Network/NetworkMessages/Server/Entity/PlayerState.cs
@@ -1,7 +1,9 @@
-public class PlayerState
+public class PlayerState : IStateSnapshot
 {
     public string PlayerId;
     public int ProcessedInputSequence;
     public float PositionX;
     public float PositionY;
+
+    int IStateSnapshot.ProcessedInputSequence => ProcessedInputSequence;
 }
