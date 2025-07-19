ae4a3d14076692b6ceee40f93c295be683e9e1e3
Quan Nguyen <minhquan2nd@gmail.com>
2025-07-19 21:51:18 +0700

    add

--- Assets/_Scripts/Network/NetworkMessages/Server/Entity/PlayerState.cs
+++ Assets/_Scripts/Network/NetworkMessages/Server/Entity/PlayerState.cs
@@ -1,9 +1,7 @@
-public class PlayerState : IStateSnapshot
+public class PlayerState
 {
     public string PlayerId;
     public int ProcessedInputSequence;
     public float PositionX;
     public float PositionY;
-
-    int IStateSnapshot.ProcessedInputSequence => ProcessedInputSequence;
 }
--- Assets/_Scripts/Network/NetworkMessages/Server/GameState/GameStateUpdate.cs
+++ Assets/_Scripts/Network/NetworkMessages/Server/GameState/GameStateUpdate.cs
@@ -1,9 +1,11 @@
 using System.Collections.Generic;
 
-public class GameStateUpdate : ServerMessage
+public class GameStateUpdate : ServerMessage, IStateSnapshot
 {
     public int ProcessedInputSequence;
     
     public PlayerState[] PlayerStates;
     public GameStateUpdate() : base(NetworkMessageTypes.Server.GameState.StateUpdate) { }
+
+    int IStateSnapshot.ProcessedInputSequence => ProcessedInputSequence;
 }
