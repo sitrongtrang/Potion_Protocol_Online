faa35170b0d8da3eb3b042e22ee4f26d1783d873
Quan Nguyen <minhquan2nd@gmail.com>
2025-07-20 10:45:37 +0700

    add

--- Assets/_Scripts/Network/NetworkMessages/Client/Ingame/PlayerInputMessage.cs
+++ Assets/_Scripts/Network/NetworkMessages/Client/Ingame/PlayerInputMessage.cs
@@ -1,20 +1,50 @@
+using System;
+using Newtonsoft.Json;
+
+[Flags]
+public enum InputFlags
+{
+    None = 0,
+    Dash = 1 << 0,
+    Attack = 1 << 1,
+    Pickup = 1 << 2,
+    Drop = 1 << 3,
+    Transfer = 1 << 4,
+    Craft = 1 << 5,
+    Submit = 1 << 6,
+}
+
+[Serializable]
 public class PlayerInputMessage : ClientMessage, IInputSnapshot
 {
+    [JsonProperty("inputSequence")]
     public int InputSequence;
 
+    [JsonProperty("moveDirX")]
     public float MoveDirX;
+    [JsonProperty("moveDirY")]
     public float MoveDirY;
-    public bool DashKeyDown;
-
-    public bool AttackKeyDown;
-
-    public bool PickupKeyDown;
-    public bool DropKeyDown;
-    public bool TransferKeyDown;
-    public bool CraftKeyDown;
-    public bool SubmitKeyDown;
+    [JsonProperty("inputFlags")]
+    public int Flags;
     public int SelectedSlot;
+
     public PlayerInputMessage() : base(NetworkMessageTypes.Client.Ingame.Input) { }
+    // public PlayerInputMessage(PlayerInputSnapshot playerInputSnapshot) : base(NetworkMessageTypes.Client.Ingame.Input)
+    // {
+    //     MoveDirX = playerInputSnapshot.MoveDir.x;
+    //     MoveDirY = playerInputSnapshot.MoveDir.y;
+
+    //     Flags = playerInputSnapshot.DashPressed ? Flags |= (int)InputFlags.Dash : Flags ;
+
+    //     Flags = playerInputSnapshot.AttackPressed ? Flags |= (int)InputFlags.Attack : Flags ;
+
+    //     Flags = playerInputSnapshot.PickupPressed ? Flags |= (int)InputFlags.Pickup : Flags ;
+    //     Flags = playerInputSnapshot.DropPressed ? Flags |= (int)InputFlags.Drop : Flags ;
+    //     Flags = playerInputSnapshot.TransferPressed ? Flags |= (int)InputFlags.Transfer : Flags ;
+    //     Flags = playerInputSnapshot.CombinePressed ? Flags |= (int)InputFlags.Craft : Flags ;
+    //     Flags = playerInputSnapshot.SubmitPressed ? Flags |= (int)InputFlags.Submit : Flags ;
+
+    // }
 
     int IInputSnapshot.InputSequence => InputSequence;
 }
